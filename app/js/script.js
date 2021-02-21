aeb0fd75fd33fd018eba016fe8e2303517f6a5c7
Jonathan Rovner <jonrovner@gmail.com>
2021-02-21 16:21:30 -0300

    completed layout and styling

--- app/js/script.js
+++ app/js/script.js
@@ -8,24 +8,25 @@ function handleRange(){
     
     if (this.value < 20) {
         pageViews.innerHTML = "10K PAGEVIEWS";
-        dollarMonth.innerHTML = "$8 / month";
+        dollarMonth.innerHTML = "$8.00 <span>/ month</span>";
     }else
     if (this.value < 40){
         pageViews.innerHTML = "50K PAGEVIEWS";
-        dollarMonth.innerHTML = "$12 / month";
+        dollarMonth.innerHTML = "$12.00 <span>/ month</span>";
     }else
     if (this.value < 60) {
         pageViews.innerHTML = "100K PAGEVIEWS";
-        dollarMonth.innerHTML = "$16 / month";
+        dollarMonth.innerHTML = "$16.00 <span>/ month</span>";
     }else
     if (this.value < 80) {
         pageViews.innerHTML = "500K PAGEVIEWS";
-        dollarMonth.innerHTML = "$24 / month";
+        dollarMonth.innerHTML = "$24.00 <span>/ month</span>";
     }else
     if (this.value < 100) {
         pageViews.innerHTML = "1M PAGEVIEWS";
-        dollarMonth.innerHTML = "$36 / month";
+        dollarMonth.innerHTML = "$36.00 <span>/ month</span>";
     }
 }
 
 range.addEventListener('change', handleRange);
+range.addEventListener('click', handleRange);
